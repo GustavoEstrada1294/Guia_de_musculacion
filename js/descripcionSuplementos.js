e452d056eb2427b8fdf8d8d7239df13cf7efa166
Gustavo Estrada <gustavo.e1294@gmail.com>
2025-01-19 17:47:13 -0600

    tablas de inforamacion de suplementos funcionable

--- js/descripcionSuplementos.js
+++ js/descripcionSuplementos.js
@@ -42,11 +42,95 @@ const tablasInformacion = {
                 </tr>
             </tbody>
     </table>
+    `,
+    preentreno:`
+    <table>
+        <caption>Informacion<span id='cerrar-tabla'>X</span></caption>
+        <thead>
+                <tr>
+                    <th>Suplemento</th>
+                    <th>Modo de uso</th>
+                    <th>Dosis recomendada</th>
+                    <th>Función</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>creatina</td>
+                    <td>Antes de entrenar</td>
+                    <td>5gr. diarios</td>
+                    <td>ayuda con la potencia fuerza y resistencia</td>
+                </tr>
+            </tbody>
+    </table>
+    `,
+    glutamina:`
+    <table>
+        <caption>Informacion<span id='cerrar-tabla'>X</span></caption>
+        <thead>
+                <tr>
+                    <th>Suplemento</th>
+                    <th>Modo de uso</th>
+                    <th>Dosis recomendada</th>
+                    <th>Función</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>creatina</td>
+                    <td>Antes de entrenar</td>
+                    <td>5gr. diarios</td>
+                    <td>ayuda con la potencia fuerza y resistencia</td>
+                </tr>
+            </tbody>
+    </table>
+    `,
+    glucosamina:`
+    <table>
+        <caption>Informacion<span id='cerrar-tabla'>X</span></caption>
+        <thead>
+                <tr>
+                    <th>Suplemento</th>
+                    <th>Modo de uso</th>
+                    <th>Dosis recomendada</th>
+                    <th>Función</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>creatina</td>
+                    <td>Antes de entrenar</td>
+                    <td>5gr. diarios</td>
+                    <td>ayuda con la potencia fuerza y resistencia</td>
+                </tr>
+            </tbody>
+    </table>
+    `,
+    carnitina:`
+    <table>
+        <caption>Informacion<span id='cerrar-tabla'>X</span></caption>
+        <thead>
+                <tr>
+                    <th>Suplemento</th>
+                    <th>Modo de uso</th>
+                    <th>Dosis recomendada</th>
+                    <th>Función</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>creatina</td>
+                    <td>Antes de entrenar</td>
+                    <td>5gr. diarios</td>
+                    <td>ayuda con la potencia fuerza y resistencia</td>
+                </tr>
+            </tbody>
+    </table>
     `
 };
 //Aañado evento a todos los botones
 document.querySelectorAll('.boton-suplementos').forEach(button =>{
-    button.addEventListener('click', function(){
+    button.addEventListener('click', function handleClick(){
         const target = this.dataset.target;
         const cambioDiv = this.closest('.texto-boton');
         const contenidoOriginal = cambioDiv.innerHTML;
@@ -59,6 +143,10 @@ document.querySelectorAll('.boton-suplementos').forEach(button =>{
         const cerrarTabla = cambioDiv.querySelector('#cerrar-tabla');
         cerrarTabla.addEventListener('click', function(){
             cambioDiv.innerHTML = contenidoOriginal;
+        
+        // Reasignar eventos al botón nuevamente
+            const botonRestaurado = cambioDiv.querySelector('.boton-suplementos');
+            botonRestaurado.addEventListener('click', handleClick);
         });
     });
 });
